5012331c083a1c92992287da9a15350e99b9309f
NikhilBhairat <104556445+NikhilBhairat@users.noreply.github.com>
2026-07-20 14:27:59 +0530

    Align Android subproject Java and Kotlin JVM targets

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -43,12 +43,41 @@ fun org.gradle.api.Project.applyNamespaceFallbackIfMissing() {
     setNamespaceMethod.invoke(androidExtension, fallbackNamespace)
 }
 
+fun org.gradle.api.Project.applyJvmTargetCompatibilityIfAndroid() {
+    val androidExtension = extensions.findByName("android") ?: return
+
+    val getCompileOptionsMethod = androidExtension::class.java.methods
+        .firstOrNull { it.name == "getCompileOptions" && it.parameterCount == 0 }
+    val compileOptions = getCompileOptionsMethod?.invoke(androidExtension)
+
+    if (compileOptions != null) {
+        compileOptions::class.java.methods
+            .firstOrNull { it.name == "setSourceCompatibility" && it.parameterCount == 1 }
+            ?.invoke(compileOptions, JavaVersion.VERSION_17)
+        compileOptions::class.java.methods
+            .firstOrNull { it.name == "setTargetCompatibility" && it.parameterCount == 1 }
+            ?.invoke(compileOptions, JavaVersion.VERSION_17)
+    }
+
+    val getKotlinOptionsMethod = androidExtension::class.java.methods
+        .firstOrNull { it.name == "getKotlinOptions" && it.parameterCount == 0 }
+    val kotlinOptions = getKotlinOptionsMethod?.invoke(androidExtension)
+
+    if (kotlinOptions != null) {
+        kotlinOptions::class.java.methods
+            .firstOrNull { it.name == "setJvmTarget" && it.parameterCount == 1 }
+            ?.invoke(kotlinOptions, "17")
+    }
+}
+
 subprojects {
     plugins.withId("com.android.library") {
         applyNamespaceFallbackIfMissing()
+        applyJvmTargetCompatibilityIfAndroid()
     }
     plugins.withId("com.android.application") {
         applyNamespaceFallbackIfMissing()
+        applyJvmTargetCompatibilityIfAndroid()
     }
 }
 
